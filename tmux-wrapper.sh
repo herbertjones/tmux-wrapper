16697d269043267825cd362e90a055d483a0b613
Herbert Jones <jones.herbert@gmail.com>
2012-08-19 11:47:44 -0500

    Added function documentation.

--- tmux-wrapper.sh
+++ tmux-wrapper.sh
@@ -1,67 +1,73 @@
 #!/bin/sh
 
-# usage:
-### Start new tmux session:
-## Note: All args are optional, Use a "" to skip argument.
+# Basic usage:
+# # Start new tmux session:
+# # Note: All args are optional, Use a "" to skip argument.
 # new_session "NEW_SESSION_NAME" "NEW_WINDOW_NAME" /STARTING/PATH "COMMANDS"
 #
-### Add a new tab and switch to it
-## Note: All args are optional, Use a "" to skip argument.
+# # Add a new tab and switch to it
+# # Note: All args are optional, Use a "" to skip argument.
 # new_window WINDOW_NAME /STARTING/PATH "COMMAND TO RUN"
 #
-### Split the window
+# # Split the window
 # vsplit PANE_NAME /STARTING/PATH PERCENT_TO_SPLIT "COMMANDS TO RUN"
 #
-### Select the top pane
+# # Select the top pane
 # tmux select-pane -D -t ${SESSION_NAME}:1
 #
-### Finish and join the new session we just created
+# # Finish and join the new session we just created
 # join_session
 
-#send_commands()
-#{
-#    # 1: Id
-#    # ...: commands
-#
-#    THISID="$1"
-#    shift
-#
-#    tmux -t "${SESSION_NAME}:${THISID}" "$@"
-#}
 
+## Send keys to current window.
+#
+#  Automatically sends enter key after sending keys.
+#  @param String  Keys to insert into window.
 send_keys()
 {
-    # 1: keys
     tmux send-keys -R -t "${SESSION_NAME}:${ID}" "$1" C-m
 }
 
+## Send keys to a specific ID for current session.
+#
+#  Same as send_keys, but the id must be passed in.
+#  @param Integer  Window ID to receive keys
+#  @param String  Keys to insert into window.
 send_keys_to_id()
 {
-    # 1: ID
-    # 2: keys
     tmux send-keys -R -t "${SESSION_NAME}:$1" "$2" C-m
 }
 
+## Set current pane to one below. Wraps around.
 pane_down()
 {
     TARGET="${SESSION_NAME}:${ID}"
     tmux select-pane -D -t "${TARGET}"
 }
+
+## Set current pane to one above. Wraps around.
 pane_up()
 {
     TARGET="${SESSION_NAME}:${ID}"
     tmux select-pane -U -t "${TARGET}"
 }
+
+## Set current pane to one to the left. Wraps around.
 pane_left()
 {
     TARGET="${SESSION_NAME}:${ID}"
     tmux select-pane -L -t "${TARGET}"
 }
+
+## Set current pane to one to the right. Wraps around.
 pane_right()
 {
     TARGET="${SESSION_NAME}:${ID}"
     tmux select-pane -R -t "${TARGET}"
 }
+
+## Set status bar on or off
+#  @param String  "on" or "off"
 statusbar()
 {
     # 1: "on" or "off"
@@ -69,12 +75,16 @@ statusbar()
     #statements
 }
 
+## Create a new "window"(More like a tab)
+#  @param String  Name of window. Will appear in status bar as the name
+#                 of the tab. Use "" to skip.
+#  @param String  Directory path to use as the current working directory
+#                 for the window. Use "" to use the current working
+#                 directory.
+#  @param String  Keys to send to the window. Can be used to start a
+#                 program automatically.
 new_window()
 {
-    # 1: name
-    # 2: working directory
-    # 3: extra send_keys
-
     # Increase ID for new window pane
     export ID=$[ ${ID} + 1 ]
     export LAST_ID=$[ $ID - 1 ]
@@ -100,22 +110,28 @@ new_window()
     fi
 }
 
+## Split a window or pane into panes.
+#  Used by hsplit and vsplit.
+#  @param Char  'h' for horizontal, 'v' for vertical.
+#  @param String  Name of window. Will appear in status bar as the name
+#                 of the tab. Use "" to skip.
+#  @param String  Directory path to use as the current working directory
+#                 for the window. Use "" to use the current working
+#                 directory.
+#  @param Number  Percentage of space the new pane will take from the
+#                 existing area.
+#  @param String  Keys to send to the window. Can be used to start a
+#                 program automatically.
 split()
 {
-    # 1: h or v
-    # 2: new path
-    # 3: percentage
-    # 4: name
-    # 5: extra send_keys
-
     export PANE_ID=$[ ${PANE_ID} + 1 ]
     TARGET="${SESSION_NAME}:${ID}"
-    PERCENTAGE=$3
+    PERCENTAGE=$4
     if [ -z "${PERCENTAGE}" ]; then
         PERCENTAGE=50
     fi
 
-    tmux split-window -$1 -p "${PERCENTAGE}" -t "${TARGET}" "cd \"$2\"; exec bash"
+    tmux split-window -$1 -p "${PERCENTAGE}" -t "${TARGET}" "cd \"$3\"; exec bash"
 
     # Add to clear list
     LEN=${#CLEAR_LIST[@]}
@@ -123,8 +139,8 @@ split()
     export CLEAR_LIST
 
     # Set inner name ( There is no useful split pane window name )
-    if [[ -n "$4" ]]; then
-        send_keys "clear;sleep 0.5;clear;echo $4"
+    if [[ -n "$2" ]]; then
+        send_keys "clear;sleep 0.5;clear;echo $2"
     fi
     # Send keys
     if [[ -n "$5" ]]; then
@@ -132,32 +148,49 @@ split()
     fi
 }
 
+## Split a window or pane horizontally.
+#  @param String  Name of window. Will appear in status bar as the name
+#                 of the tab. Use "" to skip.
+#  @param String  Directory path to use as the current working directory
+#                 for the window. Use "" to use the current working
+#                 directory.
+#  @param Number  Percentage of space the new pane will take from the
+#                 existing area.
+#  @param String  Keys to send to the window. Can be used to start a
+#                 program automatically.
 hsplit()
 {
-    # 1: name
-    # 2: new path
-    # 3: percentage
-
-    split h "$2" "$3" "$1"
+    split h "$1" "$2" "$3" "$4"
 }
 
+## Split a window or pane vertically.
+#  @param String  Name of window. Will appear in status bar as the name
+#                 of the tab. Use "" to skip.
+#  @param String  Directory path to use as the current working directory
+#                 for the window. Use "" to use the current working
+#                 directory.
+#  @param Number  Percentage of space the new pane will take from the
+#                 existing area.
+#  @param String  Keys to send to the window. Can be used to start a
+#                 program automatically.
 vsplit()
 {
-    # 1: name
-    # 2: new path
-    # 3: percentage
-
-    split v "$2" "$3" "$1"
+    split v "$1" "$2" "$3" "$4"
 }
 
+## Begin a new tmux session.
+#  This is required before anything else.
+#  @param String  Name of the session.
+#  @param String  Name of window. Will appear in status bar as the name
+#                 of the tab. Use "" to skip.
+#  @param String  Directory path to use as the current working directory
+#                 for the window. Use "" to use the current working
+#                 directory.
+#  @param String  Keys to send to the window. Can be used to start a
+#                 program automatically.
+#  @param Number  The base index tmux will use to start counting from.
 new_session()
 {
-    # 1: Session name
-    # 2: Window name
-    # 3: Starting path
-    # 4: Keys to send
-    # 5: base index
-
     export BASE_INDEX=1
     if [[ -n "$5" ]]; then
         export BASE_INDEX="$5"
@@ -201,19 +234,20 @@ new_session()
 
 }
 
+## Rename the current window
+#  @param String  The new window name.
 rename_window()
 {
-    # 1: The new window name
-
     tmux rename-window -t "${SESSION_NAME}:${ID}" "$1"
 }
 
+## Clears all history one could normally see by scrolling up.
+#  Unseen commands to set the directories and echo out the window names
+#  is visible if one scrolls up, or changes the window size. This
+#  handles removing that, which can startle one as one forgets about it.
+#  @param Number  Time to wait before executing clear.
 clear_history()
 {
-    # Clear history from cd command
-
-    # 1: Wait time. Default below.
-
     WAITTIME=0.75
     if [[ -n "$1" ]]; then
         WAITTIME="$1"
@@ -228,6 +262,10 @@ clear_history()
     send_keys "tmux kill-window -t ${SESSION_NAME}:${ID}"
 }
 
+## Join the last created session.
+#  The last created session will become active in the current window.
+#  This is not necessary if one is only setting up several sessions,
+#  such as during startup.
 join_session()
 {
     # Remove any text that could be seen by scrolling up
@@ -243,18 +281,5 @@ join_session()
     exec tmux -2 attach-session -t ${SESSION_NAME}
 }
 
+# Create the array for clear history.
 declare -a CLEAR_LIST
-
-# send_keys 'cd "/home/herbert/repositories/snakes"'
-# send_keys 'clear;echo snakes;git st'
-# vsplit /home/herbert/repositories/snakes/.build_unstripped_raw/Linux/Debug 40 "snakes build output"
-#
-# # Add more windows
-# new_window mfapi /home/herbert/repositories/libmfapi "git st"
-# new_window hub_comm /home/herbert/repositories/hub_communication "git st"
-#
-# new_window villains /home/herbert/repositories/villains 'git st'
-# vsplit /home/herbert/repositories/villains/Build/Linux 30 "Villains build"
-#
-
-# Join the tmux session
